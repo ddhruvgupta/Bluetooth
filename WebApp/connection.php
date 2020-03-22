da89051e25e0c3fccccc63609624f5c9a6b617b0
ddhruvgupta <ddhruvgupta@gmail.com>
2020-03-22 19:54:30 -0400

    updated connection file

--- WebApp/connection.php
+++ WebApp/connection.php
@@ -1,7 +1,35 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=networking_project', 'newuser', 'password');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if (isset($_SERVER['HTTP_HOST']))
+{
+    if($_SERVER['HTTP_HOST'] == "localhost")
+    {
+        $root =  realpath($_SERVER["CONTEXT_DOCUMENT_ROOT"]);
+        define("ROOT",$root."/WebApp");
+        $root = ROOT;
+
+        $pdo = new PDO('mysql:host=localhost;port=3306;dbname=aa11m73hlheg2u6', 'dgupta3', 'Welcome1!');
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // include "connection.php";
+        // include "test.php";
+        // $_SESSION['displ'] = 1;
+
+    }
+    else
+    {
+        $root =  realpath($_SERVER["CONTEXT_DOCUMENT_ROOT"]);
+        include "pdo_connection_server.php";
+        require "security.php";
+
+    }
+}else{
+	
+	$pdo = new PDO('mysql:host=localhost;port=3306;dbname=aa11m73hlheg2u6', 'dgupta3', 'Welcome1!');
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}
+
+
 
 /*
 $test_sql = "Select * from current_availability";
