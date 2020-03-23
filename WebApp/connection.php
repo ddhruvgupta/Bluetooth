bf466f289b9b8924b716438b8e8faacba11e9355
ddhruvgupta <ddhruvgupta@gmail.com>
2020-03-23 00:10:35 -0400

    connection info updated

--- WebApp/connection.php
+++ WebApp/connection.php
@@ -1,42 +1,10 @@
 <?php
 
-if (isset($_SERVER['HTTP_HOST']))
-{
-    if($_SERVER['HTTP_HOST'] == "localhost")
-    {
-        $root =  realpath($_SERVER["CONTEXT_DOCUMENT_ROOT"]);
-        define("ROOT",$root."/WebApp");
-        $root = ROOT;
-
 try {
-        $pdo = new PDO('mysql:host=localhost;port=3306;dbname=aa11m73hlheg2u6', 'dgupta3', 'Welcome1!');
+        $pdo = new PDO('mysql:host=localhost;port=3306;dbname=networking_project', 'dgupta3', 'abcd1234');
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
-}
-
-
-        // include "connection.php";
-        // include "test.php";
-        // $_SESSION['displ'] = 1;
-
-    }
-    else
-    {
-
-    	try {
-        	$pdo = new PDO('mysql:host=localhost;port=3306;dbname=aa11m73hlheg2u6', 'dgupta3', 'Welcome1!');
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		} catch (PDOException $e) {
-    		echo 'Connection failed: ' . $e->getMessage();
-		}
-
-    }
-}else{
-	
-	$pdo = new PDO('mysql:host=localhost;port=3306;dbname=aa11m73hlheg2u6', 'dgupta3', 'Welcome1!');
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 }
 
 
