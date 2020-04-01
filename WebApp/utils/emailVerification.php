919f55f2bbc6a7547fdc10dffe1c4f25ae16e7fc
ddhruvgupta <ddhruvgupta@gmail.com>
2020-04-01 14:37:36 -0400

    updates to emailVerification paths

--- WebApp/utils/emailVerification.php
+++ WebApp/utils/emailVerification.php
@@ -10,9 +10,9 @@ function emailVerify($email, $hash){
 
 
 
-require_once('../PHPMailer/PHPMailer.php');
-require_once('../PHPMailer/SMTP.php');
-require_once('../PHPMailer/Exception.php');
+require_once( __DIR__.'/../PHPMailer/PHPMailer.php');
+require_once( __DIR__.'/../PHPMailer/SMTP.php');
+require_once( __DIR__.'/../PHPMailer/Exception.php');
 
 
 echo (extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."\n";
